45c532a667286f62b56c3c92845baa7d1f4c1be8
pbravoc <pbravo7@gatech.edu>
2022-07-12 17:39:00 -0400

    updated boot+parcorr

--- figs/fig_scripts/fig6.jl
+++ figs/fig_scripts/fig6.jl
@@ -15,9 +15,9 @@ gf = groupby(df, :strain)
 percentage_border = 0.1
 ql(x) = round(quantile(x, [percentage_border])[1], digits=3) # Quantile low
 qh(x) = round(quantile(x, [1-percentage_border][1]), digits=3) # Quantile high
-gf_summary = combine(gf, [:α=>ql, :α=>qh,
-                          :β=>ql, :β=>qh,
-                          :L=>ql, :L=>qh,
+gf_summary = combine(gf, [:α=>ql, :α=>qh, :α=>mean,
+                          :β=>ql, :β=>qh, :β=>mean,
+                          :L=>ql, :L=>qh, :L=>mean,
                           :h_max=>ql, :h_max=>qh])
 print(gf_summary)
 ##
@@ -27,3 +27,53 @@ p3 = @df df boxplot(:strain, :L, outliers=false, group=:strain, ylim=(0.0, 70.0)
 p4 = @df df boxplot(:strain, :h_max, outliers=false, group=:strain, ylim=(0.0, 1100.0), title="h_max [μm]")
 plot(p1, p2, p3, p4, legend=false, grid=false, xrotation=50)
 #savefig("figs/fig5/parameters.pdf")
+##
+@df df scatter(:α, :β, group=:strain, xlim=(0, 3.0))
+##
+ols_ab = r2(lm(@formula(α_mean  ~ β_mean), gf_summary))
+ols_al = r2(lm(@formula(α_mean  ~ L_mean), gf_summary))
+ols_bl = r2(lm(@formula(β_mean  ~ L_mean), gf_summary))
+
+##
+p1 = @df gf_summary scatter(:α_mean, :β_mean, group=:strain,
+                            #series_ann = text.(:strain, :top),
+                            xerror=(:α_mean - :α_ql, :α_qh - :α_mean),
+                            yerror=(:β_mean - :β_ql, :β_qh - :β_mean), 
+                            legend=false, markersize=5, grid=false,
+                            xlabel="α[μm/hr]", ylabel="β[μm/hr]",
+                            title="α-β (R²=0.269)")
+p2 = @df gf_summary scatter(:α_mean, :L_mean, group=:strain, 
+                            xerror=(:α_mean - :α_ql, :α_qh - :α_mean),
+                            yerror=(:L_mean - :L_ql, :L_qh - :L_mean), 
+                            legend=false, markersize=5, grid=false,
+                            xlabel="α[μm/hr]", ylabel="L[μm]",
+                            title="α-L (R²=0.313)")
+p3 = @df gf_summary scatter(:β_mean, :L_mean, group=:strain, 
+                            xerror=(:β_mean - :β_ql, :β_qh - :β_mean),
+                            yerror=(:L_mean - :L_ql, :L_qh - :L_mean), 
+                            legend=false, markersize=5, grid=false,
+                            xlabel="β[μm/hr]", ylabel="L[μm]",
+                            title="β-L (R²=0.054)")##
+plot(p1, p2, p3, layout=(1,3), size=(700, 250), dpi=500)
+savefig("figs/fig5/parameters_correlation_summary.svg")
+
+##
+p1 = @df df scatter(:α, :β, group=:strain,
+                            legend=false, markersize=5, grid=false,
+                            xlabel="α[μm/hr]", ylabel="β[μm/hr]")
+##                            
+p2 = @df gf_summary scatter(:α_mean, :L_mean, group=:strain, 
+                            xerror=(:α_mean - :α_ql, :α_qh - :α_mean),
+                            yerror=(:L_mean - :L_ql, :L_qh - :L_mean), 
+                            legend=false, markersize=5, grid=false,
+                            xlabel="α[μm/hr]", ylabel="L[μm]")
+p3 = @df gf_summary scatter(:β_mean, :L_mean, group=:strain, 
+                            xerror=(:β_mean - :β_ql, :β_qh - :β_mean),
+                            yerror=(:L_mean - :L_ql, :L_qh - :L_mean), 
+                            legend=false, markersize=5, grid=false,
+                            xlabel="β[μm/hr]", ylabel="L[μm]")##
+plot(p1, p2, p3, layout=(1,3), size=(700, 250))
+##
+ols_ab = r2(lm(@formula(α  ~ β), df))
+ols_al = r2(lm(@formula(α  ~ L), df))
+ols_bl = r2(lm(@formula(β  ~ L), df))
